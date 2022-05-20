929404b3811ee4d44bc8b98428cbb1eb030596c9
Pavel Karpenko <karpenko.p.s@gmail.com>
2022-05-21 00:01:19 +0300

    UPD template `news`

--- app/resources/views/news.blade.php
+++ app/resources/views/news.blade.php
@@ -32,7 +32,38 @@
 			    </ul>
 		    </header>
 	    </div>
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+	    <section class="py-5 text-center container">
+		    <div class="row py-lg-5">
+			    <div class="col-lg-6 col-md-8 mx-auto">
+				    <h1 class="fw-light">Новости</h1>
+			    </div>
+		    </div>
+	    </section>
+	    <div class="relative bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+		    <div class="container-fluid max-w-6xl mx-auto sm:px-6 lg:px-8 album py-5 bg-light">
+			    <div class="row mb-2">
+
+
+				    <div class="col-md-6">
+					    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250
+					    position-relative bg-white">
+						    <div class="col p-4 d-flex flex-column position-static">
+							    <strong class="d-inline-block mb-2 text-primary">World</strong>
+							    <h3 class="mb-0">Featured post</h3>
+							    <div class="mb-1 text-muted">Nov 12</div>
+							    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+							    <a href="#" class="stretched-link">Continue reading</a>
+						    </div>
+						    <div class="col-auto d-none d-lg-block">
+							    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+						    </div>
+					    </div>
+				    </div>
+
+			    </div>
+		    </div>
+	    </div>
+        <div class="relative flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -46,7 +77,6 @@
                     @endif
                 </div>
             @endif
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8"></div>
         </div>
     </body>
 </html>
